30d71d7525f3b7ee5a29c6f445d3a9b2982474cb
liberalman <zscchina@163.com>
2017-06-07 09:55:41 +0800

    update github import path

--- server/gateway/gateway.go
+++ server/gateway/gateway.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
+
 	"github.com/golang/glog"
-	"github.com/oikomi/FishChatServer2/codec"
-	"github.com/oikomi/FishChatServer2/libnet"
-	"github.com/oikomi/FishChatServer2/server/gateway/conf"
-	"github.com/oikomi/FishChatServer2/server/gateway/job"
-	"github.com/oikomi/FishChatServer2/server/gateway/server"
+	"github.com/liberalman/im/codec"
+	"github.com/liberalman/im/libnet"
+	"github.com/liberalman/im/server/gateway/conf"
+	"github.com/liberalman/im/server/gateway/job"
+	"github.com/liberalman/im/server/gateway/server"
 )
 
 func init() {
@@ -22,12 +23,12 @@ func main() {
 		glog.Error("conf.Init() error: ", err)
 		panic(err)
 	}
-	gwServer := server.New()     // gateway的server对象，然后下面的libnetServe负责创建server，监听端口
-	protobuf := codec.Protobuf() // protobuf协议解析
-	if gwServer.Server, err = libnet.Serve(conf.Conf.Server.Proto, conf.Conf.Server.Addr, protobuf, 0); err != nil {//创建server，监听端口
+	gwServer := server.New()                                                                                         // gateway的server对象，然后下面的libnetServe负责创建server，监听端口
+	protobuf := codec.Protobuf()                                                                                     // protobuf协议解析
+	if gwServer.Server, err = libnet.Serve(conf.Conf.Server.Proto, conf.Conf.Server.Addr, protobuf, 0); err != nil { //创建server，监听端口
 		glog.Error(err)
 		panic(err)
 	}
 	go job.ConfDiscoveryProc() //启一个协程监控etcd服务，有配置更新的消息推送则更新本地配置
-	gwServer.Loop() //服务循环
+	gwServer.Loop()            //服务循环
 }
