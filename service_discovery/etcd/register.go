54f4e9e4a1bb322deff685b40ffc653f9c8e5d39
首超 <zscchina@163.com>
2017-06-08 10:55:35 +0800

    2
    2

--- service_discovery/etcd/register.go
+++ service_discovery/etcd/register.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	etcd "github.com/coreos/etcd/clientv3"
 	"github.com/golang/glog"
-	"github.com/oikomi/FishChatServer2/common/xtime"
+	"github.com/liberalman/im/common/xtime"
 	"golang.org/x/net/context"
 	"strings"
 	"time"
