0a19a5cac2a88df8874b6702eab89f741220df2d
首超 <zscchina@163.com>
2017-06-08 10:12:53 +0800

    Create conf.go

--- server/access/conf/conf.go
+++ server/access/conf/conf.go
@@ -3,7 +3,7 @@ package conf
 import (
 	"flag"
 	"github.com/BurntSushi/toml"
-	commconf "github.com/oikomi/FishChatServer2/common/conf"
+	commconf "github.com/liberalman/im/common/conf"
 )
 
 var (
