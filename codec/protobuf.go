3a290785b34570fe6f9458c351ebf1efb6e12151
首超 <zscchina@163.com>
2017-06-08 10:20:19 +0800

    Create protobuf.go

--- codec/protobuf.go
+++ codec/protobuf.go
@@ -3,7 +3,7 @@ package codec
 import (
 	"github.com/golang/glog"
 	"github.com/golang/protobuf/proto"
-	"github.com/oikomi/FishChatServer2/libnet"
+	"github.com/liberalman/im/libnet"
 	"io"
 	"reflect"
 )
